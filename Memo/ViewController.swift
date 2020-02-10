abb0182ed9487ad066ada6ff89652cf66584a50a
Jaehooooooon <jaehoon572@naver.com>
2020-02-10 17:08:07 +0900

    Initialize

---
+++ Memo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by 서재훈 on 2020/02/10.
+//  Copyright © 2020 서재훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
