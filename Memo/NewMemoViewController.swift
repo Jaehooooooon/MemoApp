d02dc0fa4874b95b58071fce35f2c8cd30ec67a5
Jaehooooooon <jaehoon572@naver.com>
2020-02-10 20:04:55 +0900

    새 메모

---
+++ Memo/NewMemoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NewMemoViewController.swift
+//  Memo
+//
+//  Created by 서재훈 on 2020/02/10.
+//  Copyright © 2020 서재훈. All rights reserved.
+//
+
+import UIKit
+
+class NewMemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancel() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
